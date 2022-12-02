a2e36e7272c66445882443cb11b9a834d4951e54
Bhakta Bhusan Das <113531873+BhaktaBhusanDas@users.noreply.github.com>
2022-12-02 12:50:00 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+/* main slider start */
 let slideContainer = document.querySelector(`.slideContainer`)
 let value = 0
 let left = document.getElementById(`left`)
 let right = document.getElementById(`right`)
-
+let mainSliderinterval = 4000
 
 let changeSlide = () => {
     slideContainer.style.marginLeft = `${value}%`
@@ -28,5 +29,23 @@ document.body.onload = () => {
             value = 0
         }
         changeSlide()
-    }, 4000);
+    }, mainSliderinterval);
 }
+/* main slider end */
+
+/* our partners slider start */
+let partnerSlideContainer = document.querySelector(`.partnerSlideContainer`)
+let partnerValue = 0
+let partnerSliderinterval = 1000
+setInterval(() => {
+    partnerValue -= 240
+    if (partnerValue < -2400) {
+        partnerValue = 0
+        partnerSlideContainer.style.transition = "none"
+    }
+    partnerSlideContainer.style.marginLeft = `${partnerValue}px`
+    if (partnerValue == -240) {
+        partnerSlideContainer.style.transition = ".5s cubic-bezier(0, 0, 0, 1)"
+    }
+}, partnerSliderinterval);
+/* our partners slider end */
